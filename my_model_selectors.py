ad1d404ab3bd0e3c9ab18311d79d4dbdba35bc39
ReactiveXYZ-Dev <Jackie@Jackies-MacBook-Pro.local>
2017-04-04 00:22:46 -0400

    More done

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -68,6 +68,28 @@ class SelectorBIC(ModelSelector):
     Bayesian information criteria: BIC = -2 * logL + p * logN
     """
 
+    def bic_score(self, trial_num_components):
+        """Helper to calculate BIC score
+        
+        BIC score for model with trial num of components
+        
+        Arguments:
+            trial_num_components {Integer} -- Num of comps
+        """
+        # construct model based on number of comps
+        model = self.base_model(trial_num_components)
+        # get necessary properties
+        logL = model.score(self.X, self.lengths)
+        logN = math.log(len(self.X))
+        # 
+        # Why is p calculated in this manner ?
+        # p = m^2 + km - 1 where m is number of number of states (n_components),
+        # k is 2 since we are using Gaussian(normal) distribution
+        # Source: https://rdrr.io/cran/HMMpa/man/AIC_HMM.html
+        # 
+        p = self.n_components ** 2 + 2 * self.n_components - 1
+        return -2 * logL + p * logN , model
+
     def select(self):
         """ select the best model for self.this_word based on
         BIC score for n between self.min_n_components and self.max_n_components
@@ -76,9 +98,13 @@ class SelectorBIC(ModelSelector):
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
-
+        try:
+            # finding the best model
+            best_score, model = max([ self.bic_score(n) for n in range(self.min_n_components, self.max_n_components + 1) ])
+            return model
+        except Exception as e:
+            # fallback to default
+            return self.base_model(self.n_constant)
 
 class SelectorDIC(ModelSelector):
     ''' select best model based on Discriminative Information Criterion
@@ -89,11 +115,33 @@ class SelectorDIC(ModelSelector):
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
     '''
 
+    def dic_score(self, trial_num_components):
+        """Helper to calculate DIC score
+        
+        Calculating DIC score for the model with designated number of comps
+        
+        Arguments:
+            trial_num_components {Integer} -- Number of comps
+        """
+        # construct model based on number of comps
+        model = self.base_model(trial_num_components)
+        # get necessary attributes
+        logL = model.score(self.X, self.lengths)
+        logP = [ model.score(X, lengths) for word, (X, lengths) in self.hwords.items() if word != self.this_word ]
+        M = len(logP)
+        return logL - sum(logP)/(M-1), model
+
+
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        try:
+            # find best model
+            best_score, model = max([ self.dic_score(n) for n in range(self.min_n_components, self.max_n_components + 1) ])
+            return model
+        except Exception as e:
+            # fallback to default
+            return self.base_model(self.n_constant)
 
 
 class SelectorCV(ModelSelector):
@@ -101,8 +149,44 @@ class SelectorCV(ModelSelector):
 
     '''
 
+    def cv_score(self, trial_num_components):
+        """CV Average score calculator
+        
+        Helper to calculate cv score for a model with trial number of components
+        
+        Arguments:
+            trial_num_components {Integer} -- Number of components
+        """
+        # init a score list for calculating average
+        scores = []
+        # number of splits response to sample size
+        def num_splits():
+            return math.floor(math.log(len(self.sequences), 2) + 1)
+        # init KFold spliter 
+        folded = KFold(n_splits = num_splits())
+        # get trainning and testing subsets with the splitter acting on the current word seq
+        for training_idx, testing_idx in folded.split(self.sequences):
+            # combine the training subsets and save to mode
+            self.X, self.lengths = combine_sequences(training_idx, self.sequences)
+            # init a model based on above data
+            model = self.base_model(trial_num_components)
+            # combine the testing subsets
+            X, lengths = combine_sequences(testing_idx, self.sequences)
+            # reuse the model to check performance
+            score = model.score(X, lengths)
+            # save score
+            scores.append(score)
+        # find the average value for the score
+        return np.mean(scores), model
+
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
+        
+        try:
+            # select best score
+            best_score, model = max([ self.cv_score(n) for n in range(self.min_n_components, self.max_n_components + 1) ])
+            return model
+        except:
+            # fall back to default
+            return self.base_model(self.n_constant)
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
